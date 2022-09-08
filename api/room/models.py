4420803ad500bd8938aa68fd7856fce448998f74
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-07 23:09:17 -0700

    Add files via upload

--- api/room/models.py
+++ api/room/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    length = models.IntegerField()
+    width = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
