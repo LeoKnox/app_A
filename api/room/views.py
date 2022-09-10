321dd08b43e2d154e3cd9f205bf1b66c8d9f4a31
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-09 21:59:09 -0700

    Add files via upload

--- api/room/views.py
+++ api/room/views.py
@@ -3,9 +3,10 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Room
 from .serializers import RoomSerializer
+from rest_framework import viewsets
 
 @csrf_exempt
-def room_list(request):
+def RoomViewSet(self, request):
     if request.method == 'GET':
         rooms = Room.objects.all()
         serializer = RoomSerializer(rooms, many=True)
